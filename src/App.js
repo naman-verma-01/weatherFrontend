a4c48e4116276c54f38e259bbe730c9e212b3456
naman-verma-01 <namanvermaca@gmail.com>
2023-01-29 20:50:18 +0530

    checks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { getList } from './API/PageData';
+import { useEffect, useState } from 'react';
+import CityCard from './Component/CityCard';
+
+
+import { MapContainer } from 'react-leaflet/MapContainer'
+import { TileLayer } from 'react-leaflet/TileLayer'
+import { useMap } from 'react-leaflet/hooks'
+import { Marker, Popup } from 'react-leaflet'
+
 
 function App() {
+  const [data, setData] = useState()
+  const [loading, setLoading] = useState(true)
+  const [page, setPage] = useState(1)
+
+  useEffect(() => {
+    setLoading(true)
+    getData(page)
+  }, [page])
+
+  const getListCallback = async (response) => {
+    response = await response.json()
+    console.log("response", response)
+
+    setData(response.data)
+    setLoading(false)
+  }
+  const getData = (page) => {
+    getList(page, getListCallback)
+  }
+
+  const prevPage = ()=>{
+    if(page!=1){
+      setPage(page-1)
+    }
+  }
+
+  const nextPage = ()=>{
+    if(page!=3){
+      setPage(page+1)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!loading ? data.map((element) => {
+        return <CityCard element={element} />
+      }) : <>Loading...</>}
+      <div onClick={prevPage}>Prev</div>
+      <div>Page: {page}</div>
+      <div onClick={nextPage}>Next</div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+export const serverApiUrl = "https://weather-backend-mu.vercel.app/"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
